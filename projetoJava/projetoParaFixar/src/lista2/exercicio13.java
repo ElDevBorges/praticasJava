1fd44bd56a5d5ff2aae332f0af85c152bd4b8012
eldevborges <eldevborges@gmail.com>
2025-11-25 19:39:34 -0300

    testando commit com o exercicio 13 da lista dois em processo

--- projetoJava/projetoParaFixar/src/lista2/exercicio13.java
+++ projetoJava/projetoParaFixar/src/lista2/exercicio13.java
@@ -1,9 +1,17 @@
 package lista2;
 
+import java.util.Scanner;
+
+/*13) Faça um algoritmo que leia as notas de duas avaliações de um aluno.
+Calcule a média aritmética e escrever uma mensagem que informe se o aluno
+foi ou não aprovado (considerar que nota igual ou maior que 6 o aluno é
+aprovado). Escrever também a média calculada.*/
+
 public class exercicio13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in)
+				;
 
 	}
 
