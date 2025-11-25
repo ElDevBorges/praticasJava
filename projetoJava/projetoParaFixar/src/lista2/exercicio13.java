9f2f4dd0072027b79ebca344c409a76fb302aa0c
eldevborges <eldevborges@gmail.com>
2025-11-25 20:50:30 -0300

    exercicio13, lista 2 concluído

--- projetoJava/projetoParaFixar/src/lista2/exercicio13.java
+++ projetoJava/projetoParaFixar/src/lista2/exercicio13.java
@@ -7,12 +7,48 @@ Calcule a média aritmética e escrever uma mensagem que informe se o aluno
 foi ou não aprovado (considerar que nota igual ou maior que 6 o aluno é
 aprovado). Escrever também a média calculada.*/
 
-public class exercicio13 {
 
+
+	
+public class exercicio13 {
+	
+	public static double calculateAverage (double n1, double n2, double n3) {
+		return n1 + n2 + n3 / 3;
+	};
+	
+
+	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in)
-				;
+		Scanner input = new Scanner(System.in);
+		String text;
+		double value, average;
+		double[] notes;
+		int i;
+		notes = new double[3];
+		i = 0;
+		
+		while (notes[2] == 0) {
+			System.out.print("Insira sua nota: ");
+			text = input.nextLine();
+			if (!text.trim().isEmpty()) {
+				value = Double.parseDouble(text);
+				notes[i] = value;
+				i++;
+			}
+		}
+		
+		input.close();
+		average = calculateAverage(notes[0], notes[1], notes[2]);
+		
+		System.out.printf("nota: %.2f", average);
+		if (average >= 6) {
+			System.out.println("\nAluno aprovado");
+		} else {
+			System.out.println("\nAluno reprovado");
+		}
+		
 
 	}
+	
 
 }
